2cfd5f715704cf84e6ebe83818da74b0ffd441f3
Bukovski <kmanankov@bk.ru>
2021-09-09 09:05:00 +0300

    add: template detail

--- src/App.js
+++ src/App.js
@@ -24,9 +24,45 @@ function App() {
               <span className="temperature__unit">&deg;C</span>
             </div>
           </div>
-          
+        
         </div>
       </div>
+      
+      <div className="detail">
+        <div className="detail__wrap">
+          <span className="detail__icon">
+            <i className="wi wi-cloudy"/>
+          </span>
+          <div className="detail__description">Scattered clouds</div>
+        </div>
+        
+        <div className="detail__box">
+          <div className="detail__indicator">
+            <div className="detail__info">
+              3.82
+              <span className="detail__measure">m/s</span>
+            </div>
+            <div className="detail__name">wind speed</div>
+          </div>
+          
+          <div className="detail__indicator">
+            <div className="detail__info">
+              57
+              <span className="detail__measure">%</span>
+            </div>
+            <div className="detail__name">humidity</div></div>
+          
+          <div className="detail__indicator">
+            <div className="detail__info">
+              1050
+              <span className="detail__measure">hpa</span>
+            </div>
+            <div className="detail__name">pressure</div>
+          </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
