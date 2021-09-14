8aadd65890906bb7e42def0798a79ce25656a7d2
Bukovski <kmanankov@bk.ru>
2021-09-14 11:20:00 +0300

    fix: change curent date format

--- src/units/timeConverter.js
+++ src/units/timeConverter.js
@@ -4,7 +4,7 @@ import moment from "moment";
 // Get date from Unix timestamp
 const dateFromTimestamp = (timestamp) => {
 	const date = new Date(timestamp * 1000);
-	return moment(date).format("MMMM Do");
+	return moment(date).format("ddd, MMMM DD");
 };
 
 // Get time from Unix timestamp
