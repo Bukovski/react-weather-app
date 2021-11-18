84fecc9873f6b3aba13ae29051ff8967699a817f
Bukovski <kmanankov@bk.ru>
2021-11-18 10:02:00 +0300

    add: app component change on function component

--- src/containers/app.container.jsx
+++ src/containers/app.container.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import { fetchDataWeather, fetchDataWeatherFake } from '../utils/fetchDataWeather';
 import fetchDataLocation from "../utils/fetchDataLocation";
@@ -11,89 +11,91 @@ import ThemeSwitcher from "../components/themeSwitcher.component";
 import AutocompleteContainer from "./autocomplete.container";
 
 
-class AppContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      actualTemperature: '0',
-      maxTemperature: '0',
-      minTemperature: '0',
-      date: 'Fri, May 6',
-      cityName: 'City Name',
-      windSpeed: '0',
-      humidity: '0',
-      pressure: '0',
-      weatherDescription: 'clouds',
-      weatherIcon: 'wi wi-day-cloudy',
-      temperaturesForecast: [ -10, -5, 0, 5, 10 ],
-      temperaturesForecastLabels: [ "09:00", "12:00", "15:00", "18:00", "21:00" ],
-      loaded: false,
-      error: false,
-      theme: 'default'
-    };
-  }
+export const initialValues = {
+  actualTemperature: '0',
+  maxTemperature: '0',
+  minTemperature: '0',
+  date: 'Fri, May 6',
+  cityName: 'City Name',
+  windSpeed: '0',
+  humidity: '0',
+  pressure: '0',
+  weatherDescription: 'clouds',
+  weatherIcon: 'wi wi-day-cloudy',
+  temperaturesForecast: [ -10, -5, 0, 5, 10 ],
+  temperaturesForecastLabels: [ "09:00", "12:00", "15:00", "18:00", "21:00" ],
+  loaded: false,
+  error: false,
+  theme: 'default'
+};
+
+const AppContainer = () => {
+  const [ values, setValues ] = useState(initialValues);
   
-  componentDidMount() {
-    this._getData();
-    // this._getFakeData();
-  }
+  useEffect(() => {
+    _getData();
+    // _getFakeData();
+  }, [])
   
-  _getFakeData() {
+  const _getFakeData = () => {
     setTimeout(() => {
-      this.setState({ ...fetchDataWeatherFake() });
+      setValues({
+        ...values,
+        ...fetchDataWeatherFake()
+      });
     }, 1000)
   }
   
-  _setDataWeather = async (dataWeather) => {
+  const _setDataWeather = async (dataWeather) => {
     const weatherData = await fetchDataWeather(dataWeather);
     
     if (!weatherData.error) {
-      this.setState({ ...weatherData });
+      setValues({
+        ...values,
+        ...weatherData
+      });
     }
   };
   
   // Fetch the data using the gps coordinates
-  _getData = async () => {
+  const _getData = async () => {
     const getLocation = await fetchDataLocation();
-    this._setDataWeather(getLocation)
+    _setDataWeather(getLocation)
   }
   
   
   // Fetch the data using city name from input on page
-  handleLocationChange = async (inputText) => {
-    this._setDataWeather({ cityName: inputText })
+  const handleLocationChange = async (inputText) => {
+    _setDataWeather({ cityName: inputText })
   };
   
   
-  render() {
-    const {
-      cityName, date,
-      actualTemperature, maxTemperature, minTemperature,
-      weatherDescription, weatherIcon, windSpeed, humidity, pressure,
-      temperaturesForecast, temperaturesForecastLabels,
-      loaded
-    } = this.state;
-    
-    return (
-      <Fragment>
-        <div className="main">
-          <div className="panel">
-            <AutocompleteContainer onLocationChange={ this.handleLocationChange }/>
-            <ThemeSwitcher />
-          </div>
-          
-          <Geo data={{ cityName, date }} />
-          
-          <Temperature data={{ actualTemperature, maxTemperature, minTemperature }} />
-          
-          <Detail data={{ weatherDescription, weatherIcon, windSpeed, humidity, pressure }}/>
-          
-          <Diagram data={{ temperaturesForecast, temperaturesForecastLabels, loaded }}/>
+  const {
+    cityName, date,
+    actualTemperature, maxTemperature, minTemperature,
+    weatherDescription, weatherIcon, windSpeed, humidity, pressure,
+    temperaturesForecast, temperaturesForecastLabels,
+    loaded
+  } = values;
+  
+  return (
+    <Fragment>
+      <div className="main">
+        <div className="panel">
+          <AutocompleteContainer onLocationChange={ handleLocationChange }/>
+          <ThemeSwitcher />
         </div>
-      </Fragment>
-    );
-  }
+        
+        <Geo data={{ cityName, date }} />
+        
+        <Temperature data={{ actualTemperature, maxTemperature, minTemperature }} />
+        
+        <Detail data={{ weatherDescription, weatherIcon, windSpeed, humidity, pressure }}/>
+        
+        <Diagram data={{ temperaturesForecast, temperaturesForecastLabels, loaded }}/>
+      </div>
+    </Fragment>
+  );
 }
 
 
